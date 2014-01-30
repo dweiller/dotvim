8ef6a3a7aaa31ff5737348c9d8d8c6f8582f6420
dweiller <cinidom@github.fake>
2014-01-31 00:37:56 +1100

    Update for the install.sh to cancel if a .vim config file exists.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #create symlinks
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc ~/.vimrc || fail=true
+if [ $fail }; then
+	echo "Found previous $HOME/.vim configuration file - exiting installation. Backup your .vim file (to another location) and try to reinstall"
+	exit 1
+fi
 
 #Grab the submodules
 cd ~/.vim
