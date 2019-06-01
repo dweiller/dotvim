ceeccf27edf47f92f5b3c636c68b2cfe6e067130
dweiller <4678790+dweiller@users.noreply.github.com>
2019-06-01 21:38:03 +1000

    Make install script not try to grab submodules

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 #create symlinks
 ln -s ~/.vim/vimrc ~/.vimrc || fail=true
 if [ $fail ] ; then
-	echo "Found previous $HOME/.vim configuration file - exiting installation. Backup your .vim file (to another location) and try to reinstall"
+	echo "Found previous $HOME/.vimrc configuration file - exiting installation. Remove/backup your .vimrc file and try to reinstall"
 	exit 1
 fi
-
-#Grab the submodules
-cd ~
-ln -s .vim/vimrc .vimrc
-cd ~/.vim
-git submodule update --init
