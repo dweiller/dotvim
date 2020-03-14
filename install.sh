13c411e81ee9bd553f055103d9da670edf50c3f2
dweiller <4678790+dweiller@users.noreply.github.com>
2020-03-13 20:21:11 -0700

    Remove helper (not very helpful) scripts

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#create symlinks
-ln -s ~/.vim/vimrc ~/.vimrc || fail=true
-if [ $fail ] ; then
-	echo "Found previous $HOME/.vimrc configuration file - exiting installation. Remove/backup your .vimrc file and try to reinstall"
-	exit 1
-fi
