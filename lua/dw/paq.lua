62d6b3a1b7e15ee208533523aab9270833b4980b
dweiller <4678790+dweiller@users.noreply.github.com>
2022-09-14 13:42:54 +1000

    Cleanup paq loading

--- lua/dw/paq.lua
+++ lua/dw/paq.lua
@@ -1,49 +1,49 @@
 --Bootstrap paq
-local fn = vim.fn
-
 
 local download_packages = false
+
 local function download_paq()
-    if fn.input('Download Paq? (y for yes): ') ~= 'y' then
-        return
-    end
+    local install_path = string.format('%s/site/pack/paqs/start/paq-nvim', vim.fn.stdpath('data'))
+    vim.notify('Downloading paq-nvim', vim.log.levels.INFO)
+    local out = vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
 
-    local install_path = string.format('%s/site/pack/paqs/start/paq-nvim', fn.stdpath('data'))
-
-    print 'Downloading paq-nvim'
-
-    local out = fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
-
-    print(out)
+    vim.notify(out, vim.log.levels.DEBUG)
     download_packages = true
     vim.opt.runtimepath:append(install_path)
 end
 
-if not pcall(require, 'paq') then
-    download_paq()
+local function load_paqs()
+    require 'paq' {
+        'savq/paq-nvim',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
+        'hrsh7th/cmp-path',
+        'hrsh7th/nvim-cmp',
+        'neovim/nvim-lspconfig',
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-treesitter/playground',
+        {'norcalli/nvim-colorizer.lua', opt=true},
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'b3nj5m1n/kommentary',
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        'nvim-lua/plenary.nvim',
+        'lewis6991/gitsigns.nvim',
+        'ggandor/lightspeed.nvim',
+    }
 end
 
-require 'paq' {
-    'savq/paq-nvim',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-path',
-    'hrsh7th/nvim-cmp',
-    'neovim/nvim-lspconfig',
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-treesitter/playground',
-    {'norcalli/nvim-colorizer.lua', opt=true},
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'b3nj5m1n/kommentary',
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'nvim-lua/plenary.nvim',
-    'lewis6991/gitsigns.nvim',
-    'ggandor/lightspeed.nvim',
-}
+if (not pcall(require, 'paq')) then
+    if vim.fn.input('Download Paq? (y to confirm): ') == 'y' then
+        download_paq()
+        load_paqs()
+    end
+else
+    load_paqs()
+end
 
 if download_packages then
-    print 'Downloading Paq packages, you will need to restart neovim in order for them to work'
+    vim.notify('Downloading Paq packages, you will need to restart neovim in order for them to work')
     require 'paq':install()
     _G['bootstrap-paq'] = true
 end
