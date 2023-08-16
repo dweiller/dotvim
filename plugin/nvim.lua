f7a96399a155770a2ac6b5bb282fecaa5f2da88f
dweiller <4678790+dweiller@users.noreply.github.com>
2023-08-16 17:43:50 +1000

    improve nvim-treesitter config

--- plugin/nvim.lua
+++ plugin/nvim.lua
@@ -91,9 +91,12 @@ lspconfig.rust_analyzer.setup {
 
 -- nvim-treesitter
 require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "vimdoc", "vim", "comment", "markdown", "c", "lua" },
+    sync_install = false,
+    auto_intall = true,
     highlight = {
         enable = true,
-        disable = { 'help' },
+        disable = { 'vimdoc' },
     },
     indent = { enable = true, },
     incremental_selection = { enable = true, },
