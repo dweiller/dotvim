8155d0f7eef53f0bb976aff34c98e8656d7ba23d
dweiller <4678790+dweiller@users.noreply.github.com>
2024-04-15 22:52:54 +1000

    Add highlighting of yanked text

---
+++ plugin/yank_highlight.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking test',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'Decorator',
+            timeout = 250,
+            on_visual = false,
+        })
+    end,
+})
