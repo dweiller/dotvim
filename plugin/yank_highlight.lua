a4542c84e20e97e3a58d0f58467552e0c6f3e2ab
dweiller <4678790+dweiller@users.noreply.github.com>
2025-07-18 22:59:58 +1000

    Update for rename of vim.highlight -> vim.hl

--- plugin/yank_highlight.lua
+++ plugin/yank_highlight.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking test',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank({
+        vim.hl.on_yank({
             higroup = 'Decorator',
             timeout = 250,
             on_visual = false,
